2447c3ff21cd05d7ce2d30d51038af97cffea041
Nikita Martínez <62626494+Zeniki1996@users.noreply.github.com>
2024-09-24 19:49:59 -0500

    mejora de diseño e idioma contactos

--- js/app.js
+++ js/app.js
@@ -1,43 +1,44 @@
 $(document).ready(function() {
-    var language = localStorage.getItem("language");
-  
-    // Asegura que la clase se aplique correctamente según el idioma almacenado
-    if (language === "en") {
-      $(".wrap").addClass("english");
+  var language = localStorage.getItem("language");
+
+  // Asegura que la clase se aplique correctamente según el idioma almacenado
+  if (language === "en") {
+    $(".wrap").addClass("english");
+  } else {
+    $(".wrap").removeClass("english");
+  }
+
+  // Manejo del clic para cambiar de idioma
+  $(".wrap").on("click", function() {
+    if ($(this).hasClass("english")) {
+      $(this).removeClass("english");
+      localStorage.setItem("language", "es");
     } else {
-      $(".wrap").removeClass("english");
+      $(this).addClass("english");
+      localStorage.setItem("language", "en");
     }
-  
-    // Manejo del clic para cambiar de idioma
-    $(".wrap").on("click", function() {
-      if ($(this).hasClass("english")) {
-        $(this).removeClass("english");
-        localStorage.setItem("language", "es");
-      } else {
-        $(this).addClass("english");
-        localStorage.setItem("language", "en");
-      }
-      changePageLanguage();
-    });
-    
-    function changePageLanguage() {
-      var currentPage = window.location.pathname.split("/").pop();
-      var isEnglish = $(".wrap").hasClass("english");
-  
-      var pageMap = {
-        "index.html": "indexEs.html",
-        "about.html": "AboutEs.html",
-        "Contact.html": "ContactEs.html",
-        "indexEs.html": "index.html",
-        "AboutEs.html": "about.html",
-        "ContactEs.html": "contact.html"
-      };
-  
-      if (pageMap[currentPage]) {
-        window.location.href = pageMap[currentPage];
-      }
-    }
+    changePageLanguage();
   });
+  
+  function changePageLanguage() {
+    var currentPage = window.location.pathname.split("/").pop();
+    var isEnglish = $(".wrap").hasClass("english");
+
+    var pageMap = {
+      "index.html": "indexEs.html",
+      "About.html": "AboutEs.html",
+      "Contact.html": "ContactEs.html",
+      "indexEs.html": "index.html",
+      "AboutEs.html": "About.html",
+      "ContactEs.html": "Contact.html"
+    };
+
+    if (pageMap[currentPage]) {
+      window.location.href = pageMap[currentPage];
+    }
+  }
+});
+
   
 
 
